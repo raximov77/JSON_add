213aba06ae3d64ad8ea291ed11851fcb13926dec
raximov77 <raximovikrom68@gmail.com>
2024-10-06 19:00:47 +0500

    API

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,66 @@
 import React, { useEffect, useState } from 'react';
-import Item from './components/Item'
-import './App.css'
-import useFetch from './hook/useFetch'
+import Item from './components/Item';
 import { Button, Input, Select } from 'antd';
+import './App.css';
 
-function App() {  
-  const [students, setStudents] = useState([])
-  const [teachers , setTeachers] = useState([])
-  const [statusUser, setStatusUser] = useState("1")
-  const [refreshStudent, setrefreshStudent] = useState(false)
-  const [refreshTeacher, setrefreshTeacher] = useState(false)
+function App() {
+  const [students, setStudents] = useState([]);
+  const [teachers, setTeachers] = useState([]);
+  const [statusUser, setStatusUser] = useState("1");
+  const [refreshStudent, setRefreshStudent] = useState(false);
+  const [refreshTeacher, setRefreshTeacher] = useState(false);
 
-  if (!students) {
-    return <div>Loading...</div>;
-  }
-  
-  function handleSubmit(e){
-    e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault();
     const data = {
-      name:e.target.name.value,
-      surname:e.target.surname.value,
-    }
-    if(statusUser == "1"){
-      data.study = e.target.job0rStudy.value
+      name: e.target.name.value,
+      surname: e.target.surname.value,
+    };
+
+    if (statusUser === "1") {
+      data.study = e.target.job0rStudy.value;
       fetch("http://localhost:3000/students", {
-        method:"POST",
-        body:JSON.stringify(data),
-        headers:{
-          "Content-type":"application/json"
-        }
-      }).then(res => {
-        setrefreshStudent(!refreshStudent)
-      })
-    }
-    else{
-      data.job = e.target.job0rStudy.value
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }).then(() => {
+        setRefreshStudent(!refreshStudent);
+        e.target.reset(); // Clear inputs after submission
+      });
+    } else {
+      data.job = e.target.job0rStudy.value;
       fetch("http://localhost:3000/teachers", {
-        method:"POST",
-        body:JSON.stringify(data),
-        headers:{
-          "Content-type":"application/json"
-        }
-      }).then(res => {
-        setrefreshTeacher(!refreshTeacher)
-      })
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }).then(() => {
+        setRefreshTeacher(!refreshTeacher);
+        e.target.reset(); // Clear inputs after submission
+      });
     }
   }
+
   useEffect(() => {
-    fetch("http://localhost:3000/students").then(res => res.json())
-    .then(data => setStudents(data))
-  },[refreshStudent])
+    fetch("http://localhost:3000/students")
+      .then((res) => res.json())
+      .then((data) => setStudents(data));
+  }, [refreshStudent]);
+
   useEffect(() => {
-    fetch("http://localhost:3000/teachers").then(res => res.json())
-    .then(data => setTeachers(data))
-  },[refreshTeacher])
+    fetch("http://localhost:3000/teachers")
+      .then((res) => res.json())
+      .then((data) => setTeachers(data));
+  }, [refreshTeacher]);
 
   return (
     <>
-    <form onSubmit={handleSubmit} className='w-[600px] p-5 rounded-md bg-slate-400 mx-auto mb-10 mt-5' autoComplete='off'>
-      <div className='flex items-center justify-between gap-5'>
-          <div className='flex flex-col w-[50%] gap-5'>
+      <form onSubmit={handleSubmit} className='w-3/5 p-5 rounded-md bg-gray-400 mx-auto mb-10 mt-5' autoComplete='off'>
+        <div className='flex items-center justify-between gap-5'>
+          <div className='flex flex-col w-1/2 gap-5'>
             <Select
               allowClear
               value={statusUser}
@@ -69,40 +70,36 @@ function App() {
               placeholder="Select a person"
               optionFilterProp="label"
               options={[
-                {
-                  value: '1',
-                  label: 'Student',
-                },
-                {
-                  value: '2',
-                  label: 'Teacher',
-                }
+                { value: '1', label: 'Student' },
+                { value: '2', label: 'Teacher' }
               ]}
-            />    
-              <Input name='job0rStudy' className='w-[full]' size='large'  allowClear type='text' placeholder={`Enter ${statusUser == "1" ? "study place" : "job name"}`}/>
+            />
+            <Input name='job0rStudy' className='w-full' size='large' allowClear type='text' placeholder={`Enter ${statusUser === "1" ? "study place" : "job name"}`} />
           </div>
-          <div className='flex flex-col w-[50%] gap-5'>
-              <Input name='name' className='w-[full]' size='large' allowClear type='text' placeholder='Enter name'/>
-              <Input name='surname' className='w-[full]' size='large' allowClear type='text' placeholder='Enter surname'/>
+          <div className='flex flex-col w-1/2 gap-5'>
+            <Input name='name' className='w-full' size='large' allowClear type='text' placeholder='Enter name' />
+            <Input name='surname' className='w-full' size='large' allowClear type='text' placeholder='Enter surname' />
           </div>
+        </div>
+        <Button htmlType='submit' className='w-full mt-5 bg-green-600' size='large' type='primary'>
+          Add {statusUser === "1" ? "Student" : "Teacher"}
+        </Button>
+      </form>
+
+      <div className='flex justify-center gap-20 p-10'>
+        <ul className='bg-gray-400 p-5 rounded-md space-y-4'>
+          {students.map((item) => (
+            <Item key={item.id} item={item} refreshStudent={refreshStudent} setRefreshStudent={setRefreshStudent} refreshTeacher={refreshTeacher} setRefreshTeacher={setRefreshTeacher} />
+          ))}
+        </ul>
+        <ul className='bg-gray-400 p-5 rounded-md space-y-4'>
+          {teachers.map((item) => (
+            <Item key={item.id} item={item} refreshStudent={refreshStudent} setRefreshStudent={setRefreshStudent} refreshTeacher={refreshTeacher} setRefreshTeacher={setRefreshTeacher} />
+          ))}
+        </ul>
       </div>
-      <Button htmlType='submit' className='w-full mt-5 !bg-green-600' size='large' type='primary'>Add {statusUser == "1" ? "Student" : "Teacher"}</Button>
-    </form>
-    <div className='flex justify-center gap-20 p-10'>
-      <ul className='bg-slate-400 p-5 rounded-md space-y-4'>
-        {students.map((item) => (
-            <Item refreshStudent={refreshStudent} setrefreshStudent={setrefreshStudent} setrefreshTeacher={setrefreshTeacher} refreshTeacher={refreshTeacher} key={item.id} item={item} />
-          ))}
-        </ul>
-        <ul className='bg-slate-400 p-5 rounded-md space-y-4'>
-        {teachers.map((item) => (
-            <Item refreshStudent={refreshStudent} setrefreshStudent={setrefreshStudent} setrefreshTeacher={setrefreshTeacher} refreshTeacher={refreshTeacher} key={item.id} item={item} />
-          ))}
-        </ul>
-    </div>
-     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,4 +1,49 @@
 import React from 'react';
+import { DeleteOutlined, EditOutlined, SettingOutlined } from '@ant-design/icons';
+import { Avatar, Card } from 'antd';
+const { Meta } = Card;
+
+const Item = ({ item, refreshStudent, setRefreshStudent, refreshTeacher, setRefreshTeacher }) => {
+  function handleDeleteUser(user) {
+    if (user.job) {
+      fetch(`http://localhost:3000/teachers/${user.id}`, { method: "DELETE" })
+        .then(() => setRefreshTeacher((prev) => !prev))
+        .catch((error) => console.error("Error deleting teacher:", error));
+    } else {
+      fetch(`http://localhost:3000/students/${user.id}`, { method: "DELETE" })
+        .then(() => setRefreshStudent((prev) => !prev))
+        .catch((error) => console.error("Error deleting student:", error));
+    }
+  }
+
+  return (
+    <Card
+      className='w-72 transform transition duration-300 hover:scale-105 hover:shadow-lg'
+      hoverable
+      actions={[
+        <SettingOutlined key="setting" />,
+        <EditOutlined key="edit" />,
+        <DeleteOutlined onClick={() => handleDeleteUser(item)} className='hover:text-red-500' />,
+      ]}
+    >
+      <Meta
+        avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+        title={`${item.name} - ${item.surname}`}
+        description={item?.study ? item.study : item.job}
+      />
+    </Card>
+  );
+};
+
+export default Item;
+
+
+
+
+
+/* problem with deleting*/
+
+/* export default App,,,   import React from 'react';
 import { DeleteOutlined, EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import { Avatar, Card } from 'antd';
 const { Meta } = Card;
@@ -22,12 +67,6 @@ const Item = ({item, refreshStudent, setrefreshStudent, refreshTeacher, setrefre
         style={{
           width: 300,
         }}
-       /*  cover={
-          <img
-            alt="example"
-            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-          />
-        } */
         actions={[
           <SettingOutlined key="setting" />,
           <EditOutlined key="edit" />,
@@ -42,4 +81,4 @@ const Item = ({item, refreshStudent, setrefreshStudent, refreshTeacher, setrefre
       </Card>
     )
 };
-export default Item;
+export default Item;  */
